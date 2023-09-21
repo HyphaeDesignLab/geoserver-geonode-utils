79a4653feead3c8fa4cb02b8455bcdb63049e43c
Ivan Velev <vanecat@gmail.com>
2023-09-20 17:19:51 -0700

    better upload script (helper function to echo and echo to log)

--- geonode/upload.sh
+++ geonode/upload.sh
@@ -1,4 +1,7 @@
 . debug.sh
+. helpers.sh
+geonode_upload_log=uploads.log
+
 # if no arguments, then give info and prompt user for info
 cat upload-intro.txt
 if [ "$1" = "" ]; then
@@ -16,7 +19,7 @@ if [ "$1" = "" ]; then
   echo
 fi
 
-echo ' Upload starting'
+echo_and_log ' Uploading'
 geonode_upload_results_dir=''
 # geonode_upload_results_dir will get set by uploader.sh
 . uploader.sh
@@ -24,34 +27,33 @@ geonode_upload_main "$@"
 if [ -s "$geonode_upload_results_dir/errors.log" ]; then
   echo ' there were errors in uploading... check uploads.log'
 fi
-echo ' Upload completed'
+echo_and_log ' ... done'
 
-echo ' Fetching upload jobs'
+echo_and_log ' Fetching upload jobs'
 . execution-request.sh
 geonode_execution_jobs_get
-echo '  ... done'
+echo_and_log '  ... done'
 
-echo ' Fetching datasets'
+echo_and_log
+echo_and_log ' Fetching datasets'
 . dataset.sh
 geonode_datasets_get
-echo ' ... done'
+echo_and_log ' ... done'
 
-echo >> uploads.log
-echo 'Uploaded Temp Dir with detailed logs: ' $geonode_upload_results_dir >> uploads.log
-echo >> uploads.log
+echo_and_log
+echo_and_log 'Uploaded Temp Dir with detailed logs: ' $geonode_upload_results_dir
 
-echo -n 'Uploaded Datasets: ' >> uploads.log
+echo_and_log
+echo_and_log -n 'Uploaded Datasets: '
 for geonode_upload_dataset_id in $(ls $geonode_upload_results_dir/dataset-*.json 2>/dev/null | sed -E -e 's/.+dataset-([0-9]+).json/\1/'); do
-  echo 'Dataset ID:' $geonode_upload_dataset_id >> upload-results.txt
-  echo 'URL:' https://geo2.hyphae.design/catalogue/#/dataset/$geonode_upload_dataset_id >> uploads.log
+  echo_and_log 'Dataset ID:' $geonode_upload_dataset_id
+  echo_and_log 'URL:' https://geo2.hyphae.design/catalogue/#/dataset/$geonode_upload_dataset_id
   grep -oE '"embed_url": *"[^"]+"' $geonode_upload_results_dir/dataset-$geonode_upload_dataset_id.json | sed -E -e 's/[{} "]+//g' -e 's/.*embed_url:(.+)$/\1/' > $geonode_upload_results_dir/dataset-$geonode_upload_dataset_id-embed-url.txt
-  echo 'Embed URL:' $(cat $geonode_upload_results_dir/dataset-$geonode_upload_dataset_id-embed-url.txt) >> uploads.log
-  echo >> uploads.log
+  echo_and_log 'Embed URL:' $(cat $geonode_upload_results_dir/dataset-$geonode_upload_dataset_id-embed-url.txt)
+  echo_and_log
   sleep .5
 done
-echo ' ... done'
-echo
 
-echo ' Upload done and all uploaded datasets info is in uploads.log'
-echo >> uploads.log
-echo >> uploads.log
+echo_and_log
+echo " Upload done and all uploaded datasets info/errors is in uploads.log"
+echo_and_log
